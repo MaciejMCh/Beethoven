e9b692f344827820e033189673173a0593e9cc70
MaciejMCh <maciejmch@gmail.com>
2024-08-30 13:47:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,21 @@
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-  name: "Beethoven",
-  dependencies: [
-    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 2),
-  ]
+    name: "Beethoven",
+    dependencies: [
+        .package(url: "https://github.com/vadymmarkov/Pitchy.git", from: "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Beethoven",
+            dependencies: [
+                // Add your dependencies here
+            ]
+        ),
+        .testTarget(
+            name: "BeethovenTests",
+            dependencies: ["Beethoven"]
+        ),
+    ]
 )
